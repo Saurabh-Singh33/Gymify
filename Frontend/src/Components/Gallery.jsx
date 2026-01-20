41038532ced06862ebdbc229703abcac39718010
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-01-20 23:37:13 +0530

    Worked on Pricing section

--- Frontend/src/Components/Gallery.jsx
+++ Frontend/src/Components/Gallery.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
+  const gallery = ["/img1.webp","/img2.webp","/img4.webp","/img5.jpg" , "img6.jpg","img7.webp","img8.webp","img9.webp",]
+
+  return <section className="gallery">
+    <h1>You Can!</h1>
+    <div className="images">
+      <div>
+        {
+          gallery.slice(0,3).map((element,index)=>(
+
+            <img key={index} src={element} alt="GalleryImage" />
+          ))
+        }
+      </div>
+      <div>
+        {
+          gallery.slice(3,6).map((element,index)=>(
+
+            <img key={index} src={element} alt="GalleryImage" />
+          ))
+        }
+      </div>
+      <div>
+        {
+          gallery.slice(6,9).map((element,index)=>(
+
+            <img key={index} src={element} alt="GalleryImage" />
+          ))
+        }
+      </div>
+    </div>
+     </section>
 }
 
 export default Gallery
--- Frontend/src/Components/Pricing.jsx
+++ Frontend/src/Components/Pricing.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 
 const Pricing = () => {
+  const pricing=[
+    {
+      imgUrl:"/pricing.jpg",
+      title:"Basic",
+      price:"$7/mo",
+      length:"Billed monthly"
+    },
+    {
+      imgUrl:"/pricing.jpg",
+      title:"Advance",
+      price:"$30/6mo",
+      length:"Billed every 6 months"
+    },
+    {
+      imgUrl:"/pricing.jpg",
+      title:"Premium",
+      price:"$50/yr",
+      length:"Billed yearly"
+    }
+  ]
   return (
-    <div>Pricing</div>
+    <section className="pricing">
+      <h1>Choose Your Plans</h1>
+      <div className="wrapper">
+        {
+          pricing.map(element=>{
+            return(
+              <div key={element.title} className="card">
+                <img src={element.imgUrl} alt="PricingImage" />
+                <div className="title">
+                  <h1>{element.title}</h1>
+                  <h1>Subscription</h1>
+                  <h2>{element.price}</h2>
+                  <p>{element.length}</p>
+                
+                </div>
+                </div>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
